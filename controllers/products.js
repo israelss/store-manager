a9ecf4ec510d75750952729726c19e92c138f8bd
Israel Sant'Anna <israel.santanna@gmail.com>
2022-03-30 18:21:17 -0300

    refactor: use `status_codes` helper
    Instead of use the status code directly is better to use this helper, to avoid any issues like mistyping

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,16 @@
+const { success, clientError } = require('../helpers/status_codes');
 const productsServices = require('../services/products');
 
 const getAll = async (_req, res) => {
   const result = await productsServices.getAll();
-  return res.status(200).json(result);
+  return res.status(success.OK).json(result);
 };
 
 const getById = async (req, res) => {
   const { id } = req.params;
   const result = await productsServices.getById(id);
-  if (!result) return res.status(404).json({ message: 'Product not found' });
-  return res.status(200).json(result);
+  if (!result) return res.status(clientError.NOT_FOUND).json({ message: 'Product not found' });
+  return res.status(success.OK).json(result);
 };
 
 module.exports = {
--- controllers/sales.js
+++ controllers/sales.js
@@ -1,15 +1,16 @@
+const { success, clientError } = require('../helpers/status_codes');
 const salesServices = require('../services/sales');
 
 const getAll = async (_req, res) => {
   const result = await salesServices.getAll();
-  return res.status(200).json(result);
+  return res.status(success.OK).json(result);
 };
 
 const getById = async (req, res) => {
   const { id } = req.params;
   const result = await salesServices.getById(id);
-  if (!result) return res.status(404).json({ message: 'Sale not found' });
-  return res.status(200).json(result);
+  if (!result) return res.status(clientError.NOT_FOUND).json({ message: 'Sale not found' });
+  return res.status(success.OK).json(result);
 };
 
 module.exports = {
