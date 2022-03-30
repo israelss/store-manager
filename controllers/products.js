a25bad695568b5dd77ca9c328e93209c6ac87ec9
Israel Sant'Anna <israel.santanna@gmail.com>
2022-03-30 16:43:08 -0300

    fix: add return statement to response

--- controllers/products.js
+++ controllers/products.js
@@ -8,7 +8,7 @@ const getAll = async (_req, res) => {
 const getById = async (req, res) => {
   const { id } = req.params;
   const result = await productsServices.getById(id);
-  if (!result) res.status(404).json({ message: 'Product not found' });
+  if (!result) return res.status(404).json({ message: 'Product not found' });
   return res.status(200).json(result);
 };
 
