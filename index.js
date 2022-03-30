db5347eddb0f4e86a0cd9c32d53b596be188f92e
Israel Sant'Anna <israel.santanna@gmail.com>
2022-03-30 16:30:54 -0300

    feat: add `express` to `index.js`
     - use `express.json()` middleware to handle requests with json body

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
+const express = require('express');
 
+const app = express();
+app.use(express.json());
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
