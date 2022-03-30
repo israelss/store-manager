4e6a50273bd9dcfa843c214f73fbfc45c8a9bac6
Israel Sant'Anna <israel.santanna@gmail.com>
2022-03-30 18:19:35 -0300

    feat: create `status_codes` helper

---
+++ helpers/status_codes.js
@@ -0,0 +1,14 @@
+const success = {
+  OK: 200,
+};
+
+const clientError = {
+  BAD_REQUEST: 400,
+  NOT_FOUND: 404,
+  UNPROCESSABLE_ENTITY: 422,
+};
+
+module.exports = {
+  success,
+  clientError,
+};
