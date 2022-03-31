8fe504a3e630c0c55aa77f9ef552db31d23ebd8a
Israel Sant'Anna <israel.santanna@gmail.com>
2022-03-31 16:47:33 -0300

    fix: remove keys from `connection`
     - remove `database`
     - remove `port`

--- models/connection.js
+++ models/connection.js
@@ -4,8 +4,6 @@ const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  database: 'StoreManager',
-  port: 3306,
 });
 
 module.exports = connection;
