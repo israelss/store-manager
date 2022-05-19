4b14e0009cf57516d141eb41c0213d7a13637681
Israel Sant'Anna <israel.santanna@gmail.com>
2022-05-19 18:34:15 -0300

    chore: adequate project to Trybe rules
    So I can publish on my personal GitHub!

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const salesRoutes = require('./routes/sales');
 
 const app = express();
 app.use(express.json());
-// não remova esse endpoint, e para o avaliador funcionar
+
 app.get('/', (_request, response) => {
   response.send();
 });
